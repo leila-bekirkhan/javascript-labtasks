5c36f38073fc480d07b324af2df1a2d1e54a61b1
Leila Bekirkhan <bekirkhanleila@gmail.com>
2024-07-21 19:38:49 +0200

    feat: fulfil task1

---
+++ schmedtman-js-course-udemy/task1/script.js
@@ -0,0 +1,10 @@
+// Declare variables called country, continent and population and assign their values according to your own country(population in millions).
+// Log their values to the console.
+
+let country = 'Argentina';
+let continent = 'South America';
+let population = 46;
+
+console.log(country);
+console.log(continent);
+console.log(population);
