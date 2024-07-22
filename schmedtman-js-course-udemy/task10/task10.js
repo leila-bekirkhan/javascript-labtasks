710ed54a3acdf2bab6e1715ee9dfd837113fd95a
Leila Bekirkhan <bekirkhanleila@gmail.com>
2024-07-23 00:18:51 +0200

    feat: fulfil task11

--- schmedtman-js-course-udemy/task10/task10.js
+++ schmedtman-js-course-udemy/task10/task10.js
@@ -1,4 +1,4 @@
 let x, y;
 x = y = 245 - 45 + 80;
 
-console.log(x, y);
+console.log(x, y); // 280 280 left-to-right
---
+++ schmedtman-js-course-udemy/task11/task11.js
@@ -0,0 +1,8 @@
+let nowYear = 2037;
+const ageJoanne = nowYear - 1966;
+const ageSarah = nowYear - 2017;
+
+// Average Age Calculation
+const averageAge = (ageJoanne + ageSarah) / 2;
+
+console.log(ageJoanne, ageSarah, averageAge); // 71 20 45.5
