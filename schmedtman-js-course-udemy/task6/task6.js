16f6cf79171d463d08f898204f7ec8437cd0e0e0
Leila Bekirkhan <bekirkhanleila@gmail.com>
2024-07-22 17:54:45 +0200

    feat: fulfil task7, task8

--- schmedtman-js-course-udemy/task6/task6.js
+++ schmedtman-js-course-udemy/task6/task6.js
@@ -2,3 +2,4 @@ const firstName = 'Joanne';
 const lastName = 'Rowling';
 
 console.log(firstName + ' ' + lastName);
+// Joanne Rowling
---
+++ schmedtman-js-course-udemy/task8/task8.js
@@ -0,0 +1,7 @@
+let nowYear = 2037;
+const ageJoanne = nowYear - 1966;
+const ageSarah = nowYear - 2017;
+
+console.log(ageJoanne, ageSarah); // 71 20
+console.log(ageJoanne > ageSarah); // true
+console.log(ageSarah <= 18); // false
