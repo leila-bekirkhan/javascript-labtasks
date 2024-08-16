aa6bb8ed2aa211c9a0a01ce9f208b16944d37ca6
Leila Bekirkhan <bekirkhanleila@gmail.com>
2024-08-16 19:07:17 +0200

    feat: complete task 0007

---
+++ codebasics-js-for-beginners/0007.js
@@ -0,0 +1,3 @@
+// Print the result of "the difference between five squared and the product of three and seven". Place the parentheses to follow the no-mixed-operators rule.
+
+console.log((5 ** 2) - (3 * 7)); // 4
