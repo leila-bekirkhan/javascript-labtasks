69c8cda4c68b1b0cb09dd2be1c28b30558bc3ade
Leila Bekirkhan <bekirkhanleila@gmail.com>
2024-08-16 19:01:44 +0200

    feat: complete task 0004

---
+++ codebasics-js-for-beginners/0004.js
@@ -0,0 +1,13 @@
+/*
+Given the expression 70 * 3 + 4 / 8 + 2.
+Place parentheses so that both additions (3 + 4) and (8 + 2) will be calculated first. Print the result.
+*/
+
+console.log(70 * (3 + 4) / (8 + 2)); // 49
+
+/*
+1) 3 + 4 = 7
+2) 8 + 2 = 10
+3) 70 * 7 = 490
+4) 490 / 10 = 49
+*/
